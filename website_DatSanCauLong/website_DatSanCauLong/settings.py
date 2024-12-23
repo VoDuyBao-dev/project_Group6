dc8e07f426a10f9cc8ad3dfc998c34815f3c2a4d
Mswng1265 <suongdtt1139@ut.edu.vn>
2024-12-23 09:55:30 +0700

    update

--- website_DatSanCauLong/website_DatSanCauLong/settings.py
+++ website_DatSanCauLong/website_DatSanCauLong/settings.py
@@ -81,13 +81,18 @@ from decouple import config
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='dscl'),
-        'USER': config('DB_USER', default='root'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
+print(config('DB_NAME'))
+print(config('DB_USER'))
+print(config('DB_PASSWORD'))
+print(config('DB_HOST'))
+print(config('DB_PORT'))
 
 
 
