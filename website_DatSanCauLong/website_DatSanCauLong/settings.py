1289314e28f6e065f3897d6ede0c3baec7269a95
Mswng1265 <suongdtt1139@ut.edu.vn>
2025-01-10 14:05:52 +0700

    Update setting.py

--- website_DatSanCauLong/website_DatSanCauLong/settings.py
+++ website_DatSanCauLong/website_DatSanCauLong/settings.py
@@ -88,11 +88,7 @@ DATABASES = {
         'PORT': config('DB_PORT'),
     }
 }
-print(config('DB_NAME'))
-print(config('DB_USER'))
-print(config('DB_PASSWORD'))
-print(config('DB_HOST'))
-print(config('DB_PORT'))
+
 
 
 
