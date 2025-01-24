5a12ea7d6004e83c47fa3f6353f4b9c8d0d1af63
Vi Thi Lan <lanvt6596@ut.edu.vn>
2025-01-24 14:46:41 +0700

    them stk

--- website_DatSanCauLong/user/models.py
+++ website_DatSanCauLong/user/models.py
@@ -32,7 +32,7 @@ class User(AbstractUser):
 class Customer(models.Model):
     id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
-
+    stk = models.CharField(max_length=20, null=True, blank=True)
     def __str__(self):
         return self.user.username
 
